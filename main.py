f6606c79dee7b11c596e0145546032a77aae76f7
Maksim <massering@yandex.ru>
2023-01-22 15:58:03 +0700

    Виселица с офлайн кнопками

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import telebot
 import requests
 import time
@@ -9,29 +10,29 @@ bot = telebot.TeleBot(TOKEN)
 
 
 def count_mistakes(word: str, letters: list) -> int:
-    return len(set(letters) - set(word))
+    return len(set(LETTERS) & set(letters) - set(word))
 
 
 def count_letters_remained(word: str, letters: list) -> int:
-    return len(set(word) - set(letters))
+    return len(set(LETTERS) & set(word) - set(letters))
 
 
 def make_message(word: str, letters: list) -> str:
     s = word
     print(s, letters)
-    for i in s:
+    for i in LETTERS:
         if i not in letters:
             s = s.replace(i, '_')
     mistakes = count_mistakes(word, letters)
     print(s, mistakes)
 
-    s += '\n'
+    s += '\n\n'
     s += '\n'.join(" ".join([j + STRIKETHROUGH if j in letters else j for j in i]) for i in ALF)
     s += '\n'
     s += VISELICA[mistakes]
     print(s)
 
-    return s
+    return f'<code>{s}</code>'
 
 
 @log
@@ -43,7 +44,8 @@ def rules(message: telebot.types.Message):
 Один из игроков загадывает слово — пишет на бумаге любые две буквы слова и отмечает места для остальных букв, например чертами (существует также вариант, когда изначально все буквы слова неизвестны). Также рисуется виселица с петлёй.
 Согласно традиции русских лингвистических игр, слово должно быть именем существительным, нарицательным в именительном падеже единственного числа, либо множественного числа при отсутствии у слова формы единственного числа.
 Второй игрок предлагает букву, которая может входить в это слово. Если такая буква есть в слове, то первый игрок пишет её над соответствующими этой букве чертами — столько раз, сколько она встречается в слове. Если такой буквы нет, то к виселице добавляется круг в петле, изображающий голову. Второй игрок продолжает отгадывать буквы до тех пор, пока не отгадает всё слово. За каждый неправильный ответ первый игрок добавляет одну часть туловища к виселице (обычно их 6: голова, туловище, 2 руки и 2 ноги, существует также вариант с 8 частями — добавляются ступни, а также самый длинный вариант, когда сначала за неотгаданную букву рисуются части самой виселицы).
-Если туловище в виселице нарисовано полностью, то отгадывающий игрок проигрывает, считается повешенным. Если игроку удаётся угадать слово, он выигрывает и может загадывать слово.''')
+Если туловище в виселице нарисовано полностью, то отгадывающий игрок проигрывает, считается повешенным. 
+Если игроку удаётся угадать слово, он выигрывает и может загадывать слово.''')
 
 
 @log
@@ -57,7 +59,9 @@ def start(message: telebot.types.Message):
             bot.send_message(message.chat.id, 'Окей, а теперь пришли мне слово')
         else:
             chats[chat] = {'word': message.text.upper(), 'letters': []}
-            bot.send_message(chat, make_message(*chats[chat]))
+            del people[message.from_user.id]
+            bot.send_message(message.chat.id, 'Принято', parse_mode='html')
+            bot.send_message(chat, make_message(**chats[chat]), parse_mode='html')
         return
 
     bot.send_message(message.chat.id, 'Привет всем! ' * (message.chat.id not in chats) + 'Кто будет загадывать слово?')
@@ -78,7 +82,10 @@ def begin(message: telebot.types.Message):
 @log
 def game(message: telebot.types.Message):
     if chats[message.chat.id] == 2:
-        bot.send_message(message.chat.id, 'Чуточку терпения, Ваш друг ещё не отправил мне слова')
+        if message.chat.id in people:
+            bot.send_message(message.chat.id, 'В личку, дурак!\n@viselica_bot')
+        else:
+            bot.send_message(message.chat.id, 'Чуточку терпения, Ваш друг ещё не отправил мне слова')
         bot.register_next_step_handler(message, game)
         return
 
@@ -95,14 +102,14 @@ def game(message: telebot.types.Message):
         else:
             chats[message.chat.id]['letters'] += [letter]
 
-            bot.send_message(message.chat.id, make_message(*chats[message.chat.id]))
-            mistakes = count_mistakes(*chats[message.chat.id])
+            bot.send_message(message.chat.id, make_message(**chats[message.chat.id]), parse_mode='html')
+            mistakes = count_mistakes(**chats[message.chat.id])
 
             if mistakes == len(VISELICA) - 1:
                 bot.send_message(message.chat.id, 'Угадывавшие проиграли. Поздравим загадавшего!')
                 chats[message.chat.id] = 0
 
-            elif count_letters_remained(*chats[message.chat.id]) == 0:
+            elif count_letters_remained(**chats[message.chat.id]) == 0:
                 bot.send_message(message.chat.id, 'Угадывавшие выиграли. Поздравим их!')
                 chats[message.chat.id] = 0
 
@@ -112,10 +119,6 @@ def game(message: telebot.types.Message):
     else:
         bot.send_message(message.chat.id, 'Это явно не буква.', reply_to_message_id=message.id)
         bot.register_next_step_handler(message, game)
-
-    text = make_message(*chats[message.chat.id])
-    mistakes = count_mistakes(*chats[message.chat.id])
-    bot.register_next_step_handler(message, game)
 
 
 if __name__ == '__main__':
